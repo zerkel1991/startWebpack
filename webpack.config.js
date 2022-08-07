417c1697abbd778e8af97a313dd0efda4dab6971
zerkel1991 <acorus1991@gmail.com>
2022-08-07 10:49:27 +0300

    startWp

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE__ENV || 'development';
+
+const devMode = mode === 'development'
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;  
+
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        port : 3000,
+        open: true,
+        hot:true,
+    },
+    entry:path.resolve(__dirname,'src','index.js'),
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        clean:true,
+        filename:'[name].[contenthash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template:path.resolve(__dirname,'src','index.html')
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+    })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+          {
+            test: /\.(c|sa|sc)ss$/i,
+            use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
+            "css-loader",
+            {
+                loader:'postcss-loader',
+                options:{
+                    postcssOptions: {
+                        plugins: [require('postcss-preset-env')],
+                    }
+                }
+            },
+        'sass-loader'],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      },
+}
